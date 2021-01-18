21931b031fd4ab76eb1f5f4f951bc5e9d9231d5f
Julien Leforestier <leforestier.julien@gmail.com>
2021-01-18 16:38:05 +0100

    FirstDay
    Images + Accueil

---
+++ HotelCop/js/index.js
@@ -0,0 +1,21 @@
+$(function () {
+    //datepicker
+    $('#inputDate').datepicker({
+        altField: "#datepicker",
+        closeText: 'Fermer',
+        prevText: 'Précédent',
+        nextText: 'Suivant',
+        currentText: 'Aujourd\'hui',
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+        dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+        weekHeader: 'Sem.',
+        firstDay: 1,
+        dateFormat: 'dd-mm-yy',
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    });
+});   
