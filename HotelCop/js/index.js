a57967596eb1fb2a413ce9a388b76b8aab1d6cfd
Julien Leforestier <leforestier.julien@gmail.com>
2021-01-19 10:46:04 +0100

    Structure

--- HotelCop/js/index.js
+++ HotelCop/js/index.js
@@ -1,32 +1,38 @@
-$(function () {
-    //datepicker
-    $('#inputDate').datepicker({
-        altField: "#datepicker",
-        closeText: 'Fermer',
-        prevText: 'Précédent',
-        nextText: 'Suivant',
-        currentText: 'Aujourd\'hui',
-        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-        dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
-        dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
-        weekHeader: 'Sem.',
-        firstDay: 1,
-        dateFormat: 'dd-mm-yy',
-        isRTL: false,
-        showMonthAfterYear: false,
-        yearSuffix: ""
-    });
-});   
+//datepicker
+$('#inputDate').datepicker({
+    altField: "#datepicker",
+    closeText: 'Fermer',
+    prevText: 'Précédent',
+    nextText: 'Suivant',
+    currentText: 'Aujourd\'hui',
+    monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+    monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+    dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+    dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+    dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+    weekHeader: 'Sem.',
+    firstDay: 1,
+    dateFormat: 'dd-mm-yy',
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ""
+});
 
-/*****  effet toggle page chambres.html */
-function fClassique(){
-var classique = document.getElementById("idClasImg");
-if(classique.style.display === "none"){
-    this.style.display ="block";
-}
-else{
-    classique.style.display = "none";
-}
+//fonction d'impression
+var printWindow = document.getElementsById('print');
+printWindow.addEventListener('click', print_page);
+function print_page(){
+    alert();
+    window.print();
+  }
+
+/*****  effet toggle page chambers.html */
+function fClassique() {
+    var classique = document.getElementById("idClasImg");
+    if (classique.style.display === "none") {
+        this.style.display = "block";
+    }
+    else {
+        classique.style.display = "none";
+    }
 }
